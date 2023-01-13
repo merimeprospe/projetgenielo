25c36cebb3a15be6e6cc678ffee0465e4fa183ce
merimeprospe <sprospermerime@gmail.com>
2023-01-13 11:53:29 +0100

    connection

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../provade/data.service';
+
+export let globalInfo = {
+  user: null
+}
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  user={
+    email:"",
+    password:""
+  }
+  users:any
+  error: any = false 
+  constructor(private ds:DataService, private route:Router) { }
 
   ngOnInit() {
+    this.ds.getuser().subscribe( res => {
+      console.log(res);
+      this.users = res;
+    })
+  }
+
+  connection(){
+    let i = 0
+    if(this.user.email)
+    {
+      this.users.forEach( (el:any) => {
+        if(el.email == this.user.email){
+          i=1
+          if(el.password == this.user.password)
+          {
+            globalInfo.user = el 
+            this.route.navigateByUrl('/home');
+          }
+          else{
+            this.error="mot de passe incorret"
+          }
+        }
+      });
+    }else{
+      i = 1
+      this.error="completer les information de connection"
+    }
+    if(i==0)
+    {
+      this.error="l'untilisateur n'esiste pas"
+    }
+    console.log(globalInfo.user);
   }
 
 }
